22bd49480307b69950d7fc42e88736f459413a00
psy <me@pys.im>
2021-10-23 19:36:18 +0800

    feat: chartjs integration

--- resources/js/components/SecurityHistoricalDatum.vue
+++ resources/js/components/SecurityHistoricalDatum.vue
@@ -1,23 +1,59 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
+    <div>
+        <div class="w-96 h-56">
+            <canvas id="chart" width="200" height="200"></canvas>
         </div>
     </div>
 </template>
 
 <script>
+import Chart from 'chart.js/auto';
+
 export default {
     mounted() {
-        console.log('Component mounted.')
+        let myChartElement = document.getElementById('chart');
+
+        const data = {
+            labels: [1, 2, 3, 4, 5, 6, 7],
+            datasets: [{
+                label: 'My First Dataset',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(201, 203, 207, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                    'rgb(153, 102, 255)',
+                    'rgb(201, 203, 207)'
+                ],
+                borderWidth: 1
+            }]
+        };
+
+        const config = {
+            type: 'bar',
+            data: data,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            },
+        };
+
+        // Draw the chart
+        let chartInstance = new Chart(myChartElement, config);
     }
 }
 </script>
