ef7f897a30154d6fa644ff83e793394187174949
psy <me@pys.im>
2021-10-25 00:03:41 +0800

    chore: add logo to chart page

--- resources/views/SecurityHistoricalDatum/historicalData.blade.php
+++ resources/views/SecurityHistoricalDatum/historicalData.blade.php
@@ -1,7 +1,30 @@
 <x-app-layout>
-    <security-historical-datum
-        :security='@json($security)'
-        :frequencies='@json($frequencies)'
-    >
-    </security-historical-datum>
+    <div class="flex flex-col gap-y-12">
+        <div class="max-w-full lg:max-w-sm bg-white overflow-hidden shadow rounded-lg p-5">
+            <div class="flex items-center">
+                <div class="flex-shrink-0">
+                    <img class="h-10 w-10 rounded" src="{{ asset($security->logo) }}"
+                         alt="{{ $security->name }}">
+                </div>
+                <div class="ml-5 w-0 flex-1">
+                    <dl>
+                        <dt>
+                            <div class="text-lg font-medium text-gray-900">
+                                {{ $security->name }}
+                            </div>
+                        </dt>
+                        <dd class="text-xs font-medium text-gray-500 truncate">
+                            {{ $security->ticker }} / {{ $security->currency }}
+                        </dd>
+                    </dl>
+                </div>
+            </div>
+        </div>
+
+        <security-historical-datum
+            :security='@json($security)'
+            :frequencies='@json($frequencies)'
+        >
+        </security-historical-datum>
+    </div>
 </x-app-layout>
