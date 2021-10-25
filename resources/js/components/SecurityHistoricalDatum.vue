da034e68034dc558e941531ef12de393f73fa160
psy <me@pys.im>
2021-10-25 16:09:14 +0800

    feat: calculate maximum profit and render the point with different color

--- resources/js/components/SecurityHistoricalDatum.vue
+++ resources/js/components/SecurityHistoricalDatum.vue
@@ -43,6 +43,34 @@
             <div class="bg-white rounded-lg p-4 pr-6">
                 <canvas id="chart" width="100%"></canvas>
             </div>
+            <div class="bg-white rounded-lg p-4 pr-6 mt-2">
+
+                <button type="button"
+                        @click="calculateMaximumProfit"
+                        class="inline-flex items-center px-2.5 py-1.5 mb-2 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    Calculate maximum profit
+                </button>
+                <div class="px-4 py-5 sm:p-0" v-if="maximumProfitState.calculated">
+                    <dl class="sm:divide-y sm:divide-gray-200">
+                        <div class="py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt class="text-sm font-medium text-gray-500">
+                                出售日期
+                            </dt>
+                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                {{ maximumProfitState.sellDate }}
+                            </dd>
+                        </div>
+                        <div class="py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt class="text-sm font-medium text-gray-500">
+                                利润
+                            </dt>
+                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                {{ maximumProfitState.profit }}
+                            </dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
         </div>
         <!--chart end-->
 
@@ -70,7 +98,8 @@ const state = reactive({
     frequency: 'daily',
     startDate: '2017-06-01',
     endDate: '2017-07-01',
-    chartInstance: null
+    chartInstance: null,
+    chartPointBackgroundColor: []
 });
 
 const labels = computed(() => {
@@ -86,7 +115,7 @@ const chartElement = computed(() => {
 });
 
 const chartConfig = computed(() => {
-    return {
+    let config = {
         type: 'line',
         data: {
             labels: labels.value,
@@ -99,6 +128,12 @@ const chartConfig = computed(() => {
             }]
         },
     };
+
+    if (state.chartPointBackgroundColor.length) {
+        config.data.datasets[0].pointBackgroundColor = state.chartPointBackgroundColor;
+    }
+
+    return config;
 });
 
 onMounted(() => {
@@ -139,6 +174,7 @@ const newChartInstance = () => {
 
 const handleApplyFilter = () => {
     fetchData().then(() => {
+        clearMaximumProfitState()
         redrawChart();
     });
 };
@@ -148,6 +184,13 @@ const redrawChart = () => {
     state.chartInstance = newChartInstance();
 };
 
+const clearMaximumProfitState = () => {
+    maximumProfitState.calculated = false;
+    maximumProfitState.sellDate = '';
+    maximumProfitState.profit = 0;
+    state.chartPointBackgroundColor = [];
+};
+
 const alertError = (errors) => {
     let errorString = Object.entries(errors).map(([key, value]) => {
         return value.join('\n');
@@ -155,4 +198,72 @@ const alertError = (errors) => {
 
     alert(errorString);
 };
+
+const maximumProfitState = reactive({
+    calculated: false,
+    sellDate: '',
+    profit: 0,
+});
+
+const calculateMaximumProfit = () => {
+    if (closePrice.value.length < 2) {
+        return alert('数据太少，无法计算');
+    }
+
+    // 1. 通过算法计算出必要的数据
+    let [sellIndex, profit] = maxProfitAlgorithm(closePrice.value);
+
+    if (!sellIndex) {
+        return alert('怎么卖都亏~');
+    }
+
+    // 2. 更新 maximumProfitState variable
+    updateMaximumProfitState(sellIndex, profit);
+
+    // 3. 填充 chartPointBackgroundColor 数组，高亮买入点和卖出点
+    fillChartPointBackgroundColor(sellIndex);
+
+    // 4. redraw
+    redrawChart();
+};
+
+const maxProfitAlgorithm = (data) => {
+    let minValue = data[0];
+    let maxProfitIndex = null;
+
+    let profit = 0;
+
+    for (const index in data) {
+        // 如果当前值比缓存的最小值还要小，则更新缓存
+        if (data[index] < minValue) {
+            minValue = data[index];
+        }
+
+        // 如果当前值与最小值的差值大于了缓存中的 profit，则更新缓存
+        if (data[index] - minValue > profit) {
+            profit = data[index] - minValue;
+
+            // 记录 profit 最大时的 index
+            maxProfitIndex = index;
+        }
+    }
+
+    return [maxProfitIndex, profit];
+};
+
+const updateMaximumProfitState = (sellIndex, profit) => {
+    maximumProfitState.calculated = true;
+    maximumProfitState.sellDate = state.data[sellIndex].date;
+    maximumProfitState.profit = Math.round(profit * 100) / 100;
+}
+
+const fillChartPointBackgroundColor = (sellIndex) => {
+    for (let i = 0; i < state.chartInstance.data.datasets[0].data.length; i++) {
+        if (i == sellIndex) {
+            state.chartPointBackgroundColor[i] = 'red';
+        } else {
+            state.chartPointBackgroundColor[i] = 'rgba(0, 0, 0, 0.1)';
+        }
+    }
+}
 </script>
